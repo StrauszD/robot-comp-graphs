63e4b63e11f7c3404fe5fa0e0faef51de80fbf68
Daniel Strausz <d.strausz@me.com>
2018-09-11 21:40:13 -0500

    Missing legs movement

--- Robot/cRobot.cpp
+++ Robot/cRobot.cpp
@@ -14,7 +14,10 @@
 
 #include "cRobot.hpp"
 
-
+float torDir = 1.0;
+float rShDir = 1.0;
+float rLaDir = 1.0;
+float lLaDir = -1.0;
 
 Robot::Robot()
 {
@@ -28,18 +31,18 @@ Robot::Robot()
     
     hips = new Block(7, 2.5, 4, 0.5, 0.5, 0.5, 0.0, 0.0, 0.0);            //Gray
     waist = new Block(6, 3, 3, 1, 1, 1, 0.0, 0.0, 0.0);                //White
-    torso = new Block(8, 5, 5, .627, .155, .139, 0, 1, 0.1);                //Red
+    torso = new Block(8, 5, 5, .627, .155, .139, 0, 1, 0);                //Red
     neck = new Block(2, 1.5, 2, 1, 1, 1, 0.0, 0.0, 0.0);
     head = new Block(3, 3.5, 3, .202, .329, .605, 0.0, 0.0, 0.0);                //Blue
     
-    lShoulder = new Block(3, 2.5, 3, .627, .155, .139, 0.0, 0.0, 0.0);            //Red
+    lShoulder = new Block(3, 2.5, 3, .627, .155, .139, 0.1, 0.0, 0.0);            //Red
     lUpperArm = new Block(2.3, 2, 2.3, 1, 1, 1, 0.0, 0.0, 0.0);            //White
-    lLowerArm = new Block(3, 4, 3, .627, .155, .139, 0.0, 0.0, 0.0);                //Red
+    lLowerArm = new Block(3, 4, 3, .627, .155, .139, 0.1, 0.0, 0.0);                //Red
     lHand = new Block(2.3, 2.5, 2.3, .202, .329, .605, 0.0, 0.0, 0.0);            //Blue
     
-    rShoulder = new Block(3, 2.5, 3, .627, .155, .139, 0.0, 0.0, 0.0);            //Red
+    rShoulder = new Block(3, 2.5, 3, .627, .155, .139, 0.1, 0.0, 0.0);            //Red
     rUpperArm = new Block(2.3, 2, 2.3, 1, 1, 1, 0.0, 0.0, 0.0);            //White
-    rLowerArm = new Block(3, 4, 3, .627, .155, .139, 0.0, 0.0, 0.0);                //Red
+    rLowerArm = new Block(3, 4, 3, .627, .155, .139, 0.1, 0.0, 0.0);                //Red
     rHand = new Block(2.3, 2.5, 2.3, .202, .329, .605, 0.0, 0.0, 0.0);
 }
 
@@ -103,6 +106,7 @@ void Robot::draw()
             {
                 //RIGHT SHOULDER
                 glTranslatef(-5.5, 0, 0);
+                glRotatef(rShoulder->rotAngle, rShoulder->rotX, rShoulder->rotY, 0);
                 rShoulder->draw();
                 
                 //RIGHT UPPER ARM
@@ -111,6 +115,7 @@ void Robot::draw()
                 
                 //RIGHT LOWER ARM
                 glTranslatef(0, -3, 0);
+                glRotatef(rLowerArm->rotAngle, rLowerArm->rotX, rLowerArm->rotY, 0);
                 rLowerArm->draw();
                 
                 //RIGHT HAND
@@ -130,6 +135,7 @@ void Robot::draw()
             {
                 //LEFT SHOULDER
                 glTranslatef(5.5, 0, 0);
+                glRotatef(lShoulder->rotAngle, lShoulder->rotX, lShoulder->rotY, 0);
                 lShoulder->draw();
                 
                 //LEFT UPPER ARM
@@ -138,6 +144,7 @@ void Robot::draw()
                 
                 //LEFT LOWER ARM
                 glTranslatef(0, -3, 0);
+                glRotatef(lLowerArm->rotAngle, lLowerArm->rotX, lLowerArm->rotY, 0);
                 lLowerArm->draw();
                 
                 //LEFT HAND
@@ -151,9 +158,7 @@ void Robot::draw()
     }
     glPopMatrix();
     
-    
     //LEGS
-    
     //RIGHT LEG
     glPushMatrix();
     {
@@ -193,11 +198,22 @@ void Robot::draw()
 
 void Robot::update()
 {
-    if (torso->rotAngle > 0) {
-        if (torso->rotAngle <= 7) {
-            torso->rotAngle += 0.5f;
-        }
-    } else {
-        torso->rotAngle -= 0.5f;
+    torso->rotAngle += 0.55 * torDir;
+    if (torso->rotAngle < -7 || torso->rotAngle > 7) {
+            torDir = torDir * -1.0f;
+    }
+    
+    rShoulder->rotAngle += 0.5*rShDir;
+    rLowerArm->rotAngle += 0.5*rLaDir;
+    lShoulder->rotAngle += -0.5*rShDir;
+    lLowerArm->rotAngle += 0.5*lLaDir;
+    if (rShoulder->rotAngle > 0 || rShoulder ->rotAngle < -10) {
+        rShDir = rShDir * -1.0f;
+    }
+    if (rLowerArm->rotAngle > 0 || rLowerArm->rotAngle < -10) {
+        rLaDir = rLaDir * -1.0f;
+    }
+    if (rLowerArm->rotAngle > 0 || rLowerArm->rotAngle < -10) {
+        lLaDir = lLaDir * -1.0f;
     }
 }
